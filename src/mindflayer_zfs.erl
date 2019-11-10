be3aa9143da5bd8c43b8546f78803a8b7e2521e4
Lasse G. Andersen <lasse@etableret.dk>
2019-11-10 03:57:11 +0000

    Switched from 'cmd' to 'open_port' in mindflayer_zfs

--- src/mindflayer_zfs.erl
+++ src/mindflayer_zfs.erl
@@ -12,38 +12,44 @@
 
 create(Dataset) ->
     % zfs create [-pu] [-o property=value]... filesystem
-    zfs("create " ++ Dataset).
+    zfs(["create", Dataset]).
 
 
 destroy_force(Dataset) ->
-    zfs("destroy -f " ++ Dataset).
+    zfs(["destroy", "-f", Dataset]).
 
 destroy(Dataset) ->
     % zfs destroy [-dnpRrv] snapshot[%snapname][,...]
     % zfs destroy [-fnpRrv] filesystem|volume
-    zfs("destroy " ++ Dataset).
+    zfs(["destroy", Dataset]).
 
 
 snapshot(Name) ->
     % zfs snapshot|snap [-r] [-o property=value]
-    zfs("snapshot " ++ Name).
+    zfs(["snapshot", Name]).
 
 
 clone(Snapshot, CloneName) ->
     % zfs clone [-p] [-o property=value]... snapshot filesystem|volume
-    zfs("clone " ++ Snapshot ++ " " ++ CloneName).
+    zfs(["clone", Snapshot, CloneName]).
 
 
-zfs(Cmd) ->
-    mindflayer_utils:exec("/sbin/zfs " ++ Cmd).
+zfs(Args) ->
+    Cmd = string:join(["zfs" | Args], " "),
+    io:format(user, "DEBUG CMD:~s~n", [Cmd]),
+    Port = open_port({spawn_executable, "/sbin/zfs"}, [exit_status, {line, 1024}, {args, Args}]),
+    receive
+        {Port, {exit_status, N}} ->
+            N
+    end.
 
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("test.hrl").
 create_clone_test() ->
-    "" = snapshot(?TEST_MF_ZFS_BASEJAIL_SNAPSHOT),
-    "" = clone(?TEST_MF_ZFS_BASEJAIL_SNAPSHOT, ?TEST_MF_ZFS_TESTJAIL),
-    "" = destroy(?TEST_MF_ZFS_TESTJAIL),
-    "" = destroy(?TEST_MF_ZFS_BASEJAIL_SNAPSHOT).
+    0 = snapshot(?TEST_MF_ZFS_BASEJAIL_SNAPSHOT),
+    0 = clone(?TEST_MF_ZFS_BASEJAIL_SNAPSHOT, ?TEST_MF_ZFS_TESTJAIL),
+    0 = destroy(?TEST_MF_ZFS_TESTJAIL),
+    0 = destroy(?TEST_MF_ZFS_BASEJAIL_SNAPSHOT).
 -endif.
